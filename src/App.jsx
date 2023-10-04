2da82e9435054c3e0cc801d0b34baa1f072f24e9
Ali Mohsen <ali_m_2001@hotmail.com>
2023-10-05 00:18:01 +0300

    remove unused files imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Base from "./routes/Base";
 import Home from "./routes/Home";
-import About from "./routes/About";
-import Contact from "./routes/Contact";
 import './App.css'
 
 export default function App() {
@@ -11,8 +9,6 @@ export default function App() {
       <Routes>
         <Route path="/" element={<Base />}>
           <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="contact" element={<Contact />} />
         </Route>
       </Routes>
     </BrowserRouter>
