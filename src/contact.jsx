542e12010f925cbe532e893f8872893d7414298a
Ali Mohsen <ali_m_2001@hotmail.com>
2023-10-05 16:56:55 +0300

    make this branch compatable to merge

--- src/contact.jsx
+++ src/contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Info from './Contactinfo.jsx'
 import Contactform from './Contactinput.jsx'
-import './main.css'
+import './contact.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
