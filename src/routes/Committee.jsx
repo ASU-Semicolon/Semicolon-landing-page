87ba7f28de6723e884bc1935f17ec442997f61ef
2001-hagar <1900640@eng.asu.edu.eg>
2023-10-06 15:26:08 +0300

    committee

--- src/routes/Committee.jsx
+++ src/routes/Committee.jsx
@@ -14,10 +14,25 @@ export default function Committee(props) {
 
   return (
     <>
-        <div className='committee'>
-            <h1 style={{color: "white"}}>{committee == null? "" : committee[0].content}</h1>
-            <button onClick={fetchCommittee}>click</button>
-        </div>
+        <section className='main'>
+          <div className="name">
+            <h1 className='title line'>{committee == null? "" : committee[0].content}</h1>
+            <img src={committee == null? "" : committee[0].content} alt=""  className='icon'/>
+            </div>
+            
+            <p className='description'>{committee == null? "" : committee[0].content}</p>
+            <h1 className='title'> <span className='line'>Who are</span> they</h1>
+            <p className='description'>{committee == null? "" : committee[0].content}</p>
+            <h1 className='title'> <span className='line'>what th</span>ey do</h1>
+            <p className='description'>{committee == null? "" : committee[0].content}</p>
+            <h1 className='title'> <span className='line'>want to</span> join them</h1>
+            <p className='description'>{committee == null? "" : committee[0].content}</p>
+
+
+
+
+            <button  className='btn' onClick={fetchCommittee}>click</button>
+        </section>
     </>
   )
 }
