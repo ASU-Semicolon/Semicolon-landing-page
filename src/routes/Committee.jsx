fe9a964e0876a3a9a6e5f3829ad82a6504d80e97
2001-hagar <1900640@eng.asu.edu.eg>
2023-10-06 17:13:54 +0300

    second

--- src/routes/Committee.jsx
+++ src/routes/Committee.jsx
@@ -1,38 +1,39 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import './committee.css'
 
 export default function Committee(props) {
 
     const [committee, setCommittee] = useState();
 
-    async function fetchCommittee() {
-        let response = await fetch('https://api.quotable.io/quotes/random');
-        let user = await response.json();
-        setCommittee(user);
-        console.log(committee);
-    }
+    useEffect(() => {
+      async function fetchData () {
+      let response = await fetch('https://api.quotable.io/quotes/random');
+      let user = await response.json();
+      setCommittee(user);
+      }
+      fetchData();
+      }, [])
+      
 
   return (
     <>
-        <section className='main'>
-          <div className="name">
-            <h1 className='title line'>{committee == null? "" : committee[0].content}</h1>
-            <img src={committee == null? "" : committee[0].content} alt=""  className='icon'/>
+        {committee == null? "" :<section className='committee-main'> 
+          <div className="committee-name">
+            <h1 className='committee-title committee-title-underline'>{committee[0].content}</h1>
+            <img src= {committee[0].content} alt=""  className='committee-icon'/>
             </div>
-            
-            <p className='description'>{committee == null? "" : committee[0].content}</p>
-            <h1 className='title'> <span className='line'>Who are</span> they</h1>
-            <p className='description'>{committee == null? "" : committee[0].content}</p>
-            <h1 className='title'> <span className='line'>what th</span>ey do</h1>
-            <p className='description'>{committee == null? "" : committee[0].content}</p>
-            <h1 className='title'> <span className='line'>want to</span> join them</h1>
-            <p className='description'>{committee == null? "" : committee[0].content}</p>
 
-
-
-
-            <button  className='btn' onClick={fetchCommittee}>click</button>
+            <p className='committee-description'>{committee[0].content}</p>
+            <h1 className='committee-title'> <span className='committee-title-underline'>Who are</span> they</h1>
+            <p className='committee-description'>{committee[0].content}</p>
+            <h1 className='committee-title'> <span className='committee-title-underline'>what th</span>ey do</h1>
+            <p className='committee-description'>{committee[0].content}</p>
+            <h1 className='committee-title'> <span className='committee-title-underline'>want to</span> join them</h1>
+            <p className='committee-description'>{committee[0].content}</p>
+            <button className='committee-btn'> <a href="/" className='join-committee'>join us</a></button> 
         </section>
+}
     </>
   )
 }
