ede1f666c479099eeef715d5596baf65f10bf44d
Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>
2023-10-09 21:44:20 +0300

    feat: update dropbox component
    make the component more dynamic and reusable by passing its
    options in props

--- src/components/forminput/dropbox.jsx
+++ src/components/forminput/dropbox.jsx
@@ -1,18 +1,20 @@
-import './forminput.css'
-import './dropbox.css'
+import "./forminput.css";
+import "./dropbox.css";
 
 export default function Dropdownmenu(props) {
     return (
-    <>
-     <div className='input-field'>
-    <label htmlFor={props.id}>{props.label}</label>
-    <select name={props.name} id={props.id} required>
-        <option className='default' value=''hidden>{props.placeholder}</option>
-        <option value="Web">Web</option>
-        <option value="Embedded">Embedded</option>
-        <option value="Python">Python</option>
-    </select>
-</div>
-    </>
-    )
+        <>
+            <div className="input-field">
+                <label htmlFor={props.id}>{props.label}</label>
+                <select name={props.name} id={props.id} required>
+                    <option className="default" value={props.placeholder} hidden>
+                        {props.placeholder}
+                    </option>
+                    {props.options.map((option) => {
+                        return <option value={option}>{option}</option>;
+                    })}
+                </select>
+            </div>
+        </>
+    );
 }
