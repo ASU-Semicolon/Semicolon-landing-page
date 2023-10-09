74782440c7165e9f98b9f8bb2e0444443cdb7819
Ali Mohsen <ali_m_2001@hotmail.com>
2023-10-09 11:55:27 +0300

    Landing page base (#6)
    * add react router and home page
    
    * added home and about us in landing page
    
    * removed unused files
    
    * remove unused files imports
    
    * Contact Us Page
    
    * removed illustration components
    
    * changing the illustrations
    
    * contact us Design
    
    * make this branch compatable to merge
    
    * merged ContactUs with Home&About
    
    * first commit
    
    * make component for contact card
    
    * fix some issues
    
    * fixed styling and removed bootstrap
    
    * merge base with landing page
    
    * added committee page and fetch data
    
    * fix small mistake
    
    * fix small mistake
    
    * fix the pull request comments
    
    * committee
    
    * second
    
    * added committee cards
    
    * Form Page design
    
    * base-mobile
    
    * added links to committees pages
    
    * first- version
    
    * FormPage Edits & Mob design
    
    * formpage edits
    
    * formpage edits 2
    
    * fix scroll issues and link to the form
    
    * fix small issues
    
    * base-mob
    
    * base-mobile
    
    * makeing nav bar compatable with mobile and desktop
    
    * finishing nav bar merge and functionality
    
    ---------
    
    Co-authored-by: Shorouk <shorouktamer257@gmail.com>
    Co-authored-by: 2001-hagar <1900640@eng.asu.edu.eg>
    Co-authored-by: Ahmed Khaled <80486237+Ahmed-Khaled24@users.noreply.github.com>

--- src/routes/Base.jsx
+++ src/routes/Base.jsx
@@ -1,35 +1,49 @@
 import { Outlet, Link } from "react-router-dom";
 import FooterLink from '../components/footerLink/FooterLink'
-import './base.css'
+import { useEffect, useRef } from "react";
+import { scrollContext } from "../Context";
+import './base-css/nav.css'
+import './base-css/footer.css'
+import './base-css/logo.css'
 
 export default function Base() {
+
+
+  const scrollRef = useRef(new Array());
+  const myDiv = useRef();
+
+  const handleScroll = (event, num) => {
+    const buttons = event.target.parentElement.parentElement.children
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].children[0].classList.remove("selected");
+    }
+    event.target.classList.add("selected");
+    scrollRef.current[num].scrollIntoView({behavior: 'smooth'});
+  }
+  
+  const handleResize = () => {
+    if (window.innerWidth > 760) {
+      myDiv.current.classList.remove("collapse");
+      myDiv.current.setAttribute("data-bs-toggle", "");
+      myDiv.current.setAttribute("data-bs-target", "");
+    } else {
+      myDiv.current.classList.add("collapse");      
+      myDiv.current.setAttribute("data-bs-toggle", "collapse");
+      myDiv.current.setAttribute("data-bs-target", "#navbarToggleExternalContent");
+    }
+  }
+  
+  useEffect(() => {
+    window.addEventListener("resize", handleResize);
+  }, [])
+  
   return (
     <>
+      
       <header>
-        <div className="main-logo">
-          <img src="src/assets/home-graphics/logo.svg" width="130px" alt="" />
-          <div className="main-logo-text">
-            <h2 className='main-logo-title'>Sem,Colon
-              <div className="orange-circle"></div>
-            </h2>
-            <p className='main-logo-slogan'>Debug Your <span className='orange-text'>Soul.</span></p>
-          </div>
-        </div>
-        
-        <nav className='nav-bar'>
-          <button className='nav-button selected'><a href="#home">Home</a></button>
-          <button className='nav-button'><a href="#about">About Us</a></button>
-          <button className='nav-button'><a href="#committee">Committees</a></button>
-          <button className='nav-button'><a href="#contact">Contact Us</a></button>
-        </nav>
-      </header>
-
-      <Outlet />
-
-      <footer>
-        <div className="main-footer">
+        <Link to="/">
           <div className="main-logo">
-            <img src="src/assets/home-graphics/logo.svg" width="130px" alt="" />
+            <img src="src/assets/home-graphics/logo.svg" width="90px" alt="" />
             <div className="main-logo-text">
               <h2 className='main-logo-title'>Sem,Colon
                 <div className="orange-circle"></div>
@@ -37,6 +51,54 @@ export default function Base() {
               <p className='main-logo-slogan'>Debug Your <span className='orange-text'>Soul.</span></p>
             </div>
           </div>
+        </Link>
+        
+        <div className="nav-wrapper">
+          <nav class="navbar navbar-dark mobile">
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarToggleExternalContent" aria-controls="navbarToggleExternalContent" aria-expanded="false" aria-label="Toggle navigation">
+                <div class="nav-icon"></div>
+                <div class="nav-icon"></div>
+              </button>
+          </nav>
+
+          <div  id="navbarToggleExternalContent" ref={myDiv}>
+            <nav className='nav-bar'>
+              <Link to="/">
+                <button className='nav-button selected' onClick={(event) => {handleScroll(event, 0)}}>Home</button>
+              </Link>
+              <Link to="/">
+                <button className='nav-button' onClick={(event) => {handleScroll(event, 1)}}>About Us</button>
+              </Link>
+              <Link to="/">
+                <button className='nav-button' onClick={(event) => {handleScroll(event, 2)}}>Committees</button>
+              </Link>
+              <Link to="/">
+                <button className='nav-button' onClick={(event) => {handleScroll(event, 3)}}>Contact Us</button>
+              </Link>
+            </nav>
+          </div>
+        </div>
+
+      </header>
+      
+      <scrollContext.Provider value={{scrollRef}} >
+        <Outlet />
+      </scrollContext.Provider>
+
+      <footer>
+        <div className="main-footer">
+          <Link to="/">
+            <div className="main-logo">
+              <img src="src/assets/home-graphics/logo.svg" width="90px" alt="" />
+              <div className="main-logo-text">
+                <h2 className='main-logo-title'>Sem,Colon
+                  <div className="orange-circle"></div>
+                </h2>
+                <p className='main-logo-slogan'>Debug Your <span className='orange-text'>Soul.</span></p>
+              </div>
+            </div>
+          </Link>
+
           <div className="footer-links-container">
             <div className="footer-links-list">
               <p className='footer-list-title'>FOLLOW US</p>
@@ -61,24 +123,30 @@ export default function Base() {
             </div>
             <div className="footer-links-list">
               <p className='footer-list-title'>Navigate</p>
-              <FooterLink
-                href="#home"
-                target=''
-                url="src/assets/footer/Home.svg"
-                text="Home"
-              />
-              <FooterLink 
-                href="#about"
-                target=''
-                url="src/assets/footer/About.svg"
-                text="About"
-              />
-              <FooterLink 
-                href="#committee"
-                target=''
-                url="src/assets/footer/People.svg"
-                text="Committees"
-              />
+              <Link to="/">
+                <FooterLink
+                  handleScroll = {(event) => {handleScroll(event, 0)}}
+                  target=''
+                  url="src/assets/footer/Home.svg"
+                  text="Home"
+                />
+              </Link>
+              <Link to="/">
+                <FooterLink 
+                  handleScroll = {(event) => {handleScroll(event, 1)}}
+                  target=''
+                  url="src/assets/footer/About.svg"
+                  text="About"
+                />
+              </Link>
+              <Link to="/">
+                <FooterLink 
+                  handleScroll = {(event) => {handleScroll(event, 2)}}
+                  target=''
+                  url="src/assets/footer/People.svg"
+                  text="Committees"
+                />
+              </Link>
             </div>
           </div>
         </div>
@@ -86,6 +154,7 @@ export default function Base() {
           <p className='copyrights-text'>All Rights Reserved © SemiColon Club 2024</p>
         </div>
       </footer>
-    </>
+  
+  </>
   )
 }
