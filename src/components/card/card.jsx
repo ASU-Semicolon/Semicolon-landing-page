87fe3f0518cff83817f90a82fd7aaf3081aa11ff
Ali Mohsen <ali_m_2001@hotmail.com>
2024-02-18 23:45:51 +0200

    feat: adding open now banner on committee cards

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -7,13 +7,20 @@ export default function Card(props) {
         <>
             <Link className="card-container" to={"/" + props.href}>
                 <div className="card-text-container">
+                    {props.disabled ? 
                     <h3 className="card-title">{props.title}</h3>
+                    :
+                    <>
+                        <h3 className="card-title">{props.title + "\n" + props.subtitle}</h3>
+                        <p className="card-banner">Open Now</p>    
+                    </>
+                    }
                     <div className="mobile-card-description">
                         <p className="card-description">{props.description}</p>
                         <img
                             className="card-image"
                             width="70px"
-                            src={getCommitteeIcon(props.title)}
+                            src={(props.disabled || !props.subtitle) ? getCommitteeIcon(props.title) : getCommitteeIcon(props.title+" "+props.subtitle)}
                             alt=""
                         />
                     </div>
