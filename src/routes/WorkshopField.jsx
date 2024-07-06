403246d70c27bcdaa1369ad970d0ac35af0e181e
Ahmed Khaled <ahmedkhaled.m24@gmail.com>
2024-07-06 12:18:10 +0300

    chore: add tab logo

--- src/routes/WorkshopField.jsx
+++ src/routes/WorkshopField.jsx
@@ -1,73 +1,60 @@
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import "./event.css";
-import cards from "./data"
+import cards from "./data";
 import { IoMdTime } from "react-icons/io";
 import { IoLocationSharp } from "react-icons/io5";
 
 const WorkshopField = () => {
     const { fieldName } = useParams();
-    const card = cards.find(card => {
+    const card = cards.find((card) => {
         return card.title == fieldName;
-    })
+    });
 
     return (
         <div className="hackathon-page">
-            <div className="header">
-                {card.title}
-            </div>  
-            {card.breif ?
+            <div className="header">{card.title}</div>
+            {card.breif ? (
                 <section>
                     <p className="paragraph">&emsp; {card.breif}</p>
                 </section>
-            :
+            ) : (
                 <section>
                     <h1> Description </h1>
                     <p className="paragraph">&emsp; {card.description}</p>
                 </section>
-            }
-            {card.objectives && 
+            )}
+            {card.objectives && (
                 <section>
                     <h1> Objectives </h1>
                     <ul>
-                        {card.objectives.map(
-                            (item, index) => {
-                                return (
-                                    <li key={index}>{item}</li>
-                                );
-                            }
-                        )}
+                        {card.objectives.map((item, index) => {
+                            return <li key={index}>{item}</li>;
+                        })}
                     </ul>
                 </section>
-            }
+            )}
             <section>
                 <h1> Content </h1>
                 <ul>
-                    {card.content.map(
-                        (item, index) => {
-                            return (
-                                <li key={index}>{item}</li>
-                            );
-                        }
-                    )}
+                    {card.content.map((item, index) => {
+                        return <li key={index}>{item}</li>;
+                    })}
                 </ul>
             </section>
             <section>
                 <h1> Prerequisites </h1>
                 <ul>
-                    {card.prerequisites.map(
-                        (item, index) => {
-                            return (
-                                <li key={index}>{item}</li>
-                            );
-                        }
-                    )}
+                    {card.prerequisites.map((item, index) => {
+                        return <li key={index}>{item}</li>;
+                    })}
                 </ul>
             </section>
             <section>
                 <h1> Number of Sessions </h1>
                 <p className="paragraph">
-                    &emsp; A total of {card.duration_in_sessions} sessions with {card.sessions_per_week} sessions a week.
+                    &emsp; A total of {card.duration_in_sessions} sessions with{" "}
+                    {card.sessions_per_week} sessions a week.
                 </p>
             </section>
             {/* <section>
@@ -76,7 +63,7 @@ const WorkshopField = () => {
                 <p className="paragraph">&emsp; <IoLocationSharp/> {card.location}</p>
             </section> */}
             <section>
-                <Link to="/form/workshops" target="_blank">
+                <Link to="/form/workshops">
                     <button className="applyButton">Apply Now</button>
                 </Link>
             </section>
